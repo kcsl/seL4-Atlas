cbc77f8d1fbd28e447f003850485059b4dc7d61d
Ahmed Tamrawi <ahmedtamrawi@ensoftcorp.com>
2018-11-22 00:38:42 +0200

    First commit

---
+++ com.kcsl.sel4/include/build/autoconf/autoconf.h
@@ -0,0 +1,5 @@
+#define AUTOCONF_INCLUDED
+#ifndef AUTOCONF_kernel_autoconf_H
+#define AUTOCONF_kernel_autoconf_H
+#include <build/gen_config/kernel/gen_config.h>
+#endif
---
+++ com.kcsl.sel4/include/build/generated/plat_mode/machine/hardware_gen.h
@@ -0,0 +1,8 @@
+#ifndef _HOME_AHMED_DESKTOP_BUILD_GENERATED_PLAT_MODE_MACHINE_HARDWARE_GEN_H
+#define _HOME_AHMED_DESKTOP_BUILD_GENERATED_PLAT_MODE_MACHINE_HARDWARE_GEN_H
+
+#include <assert.h>
+#include <config.h>
+#include <stdint.h>
+#include <util.h>
+#endif
